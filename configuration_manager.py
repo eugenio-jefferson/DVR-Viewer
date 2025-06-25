3a53c20276294c09e810b345953eb4637a98ec6e
Eugênio Jefferson <eugeniojeffersonsantana@gmail.com>
2025-06-25 17:39:03 -0300

    :sparkles: feat: property in configuration manager

--- configuration_manager.py
+++ configuration_manager.py
@@ -42,6 +42,36 @@ class ConfigurationManager:
     def is_empty(self):
         return not self.config_file_path.exists() or os.path.getsize(self.config_file_path) == 0
 
+    @property
+    def program_name(self):
+        return self.settings['program_data'].get('name')
+
+    @property
+    def dvr_url(self):
+        return self.settings['user_data'].get('dvr_url')
+
+    @dvr_url.setter
+    def dvr_url(self, value):
+        self.settings['user_data']['dvr_url'] = value
+        self.save_config()
+
+    @property
+    def exists_dvr_url(self):
+        return self.dvr_url != None and self.dvr_url != ""
+
+    @property
+    def last_username(self):
+        return self.settings['user_data'].get('last_username')
+
+    @last_username.setter
+    def last_username(self, value):
+        self.settings['user_data']['last_username'] = value
+        self.save_config()
+
+    @property
+    def exists_last_username(self):
+        return self.last_username is not None and self.last_username != ""
+
     def save_config(self):
         try:
             with open(self.config_file_path, 'w', encoding='utf-8') as file:
